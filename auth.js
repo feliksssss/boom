224738b710134cb7fe1f1a2abd12be40207c576b
feliks <feliksshafir@gmail.com>
2022-07-24 16:15:27 +0300

    change ls files

--- auth.js
+++ auth.js
@@ -1,1 +1,9 @@
 console.log("Viktor");
+console.log(05);
+console.log('Добро '+'пожаловать '+'на курс');
+
+document.getElementById("out").innerHTML = 340;
+
+document.querySelector('#out').innerHTML = 19;
+document.querySelector('.header').innerHTML = `12`;
+document.querySelector('.year').innerHTML = 2002;
